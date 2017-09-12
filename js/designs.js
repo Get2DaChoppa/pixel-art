9943cc9c5cb7025d493b717847cb3fd11c0e353a
Sotiris Bimpas <s.g.bibas@gmail.com>
2017-09-13 02:03:18 +0300

    Add files via upload

--- js/designs.js
+++ js/designs.js
@@ -1,31 +1,29 @@
-$(function() {
-    $("#make_grid").click(function makeGrid() {
-        $("tr").remove();
-        $("#pixel_canvas").css("background-color","white");
-        const height = $("#input_height").val();
-        const width = $("#input_width").val();
-        const pixel = $("#input_pixel").val();
-        for (var x = 1; x<=height ; x++){
-            $("#pixel_canvas").append("<tr> </tr>");
-            for (var i = 1; i<=width ; i++){
+const sizePicker = document.querySelector("#sizePicker");
+sizePicker.addEventListener("submit", function(e) {
+    e.preventDefault();
+    makeGrid ()
+});
+
+function makeGrid () {
+    $("tr").remove();
+    $("#pixel_canvas").css("background-color","white");
+    const height = $("#input_height").val();
+    const width = $("#input_width").val();
+    const pixel = $("#input_pixel").val();
+    for (var x = 1; x<=height ; x++){
+        $("#pixel_canvas").append("<tr> </tr>");
+        for (var i = 1; i<=width ; i++){
             $("tr").last().append("<td> </td>");
-            };
-        }
-        $("tr").css("height",pixel);
-        $("td").css("width",pixel);
-    });
-});
+        };
+    }
+    $("tr").css("height",pixel);
+    $("td").css("width",pixel);
+}
 
 $(function() {
-    $("#pixel_canvas").click(function (){
+    $("#pixel_canvas").click(function (e){
         let color = $("#colorPicker").val();
-        $(event.target).css("background-color",color);
-    });
-});
-
-$(function() {
-    $("#pixel_canvas").click(function (){
-        let color = $("#colorPicker").val();
-        $(event.target).css("background-color",color);
+        e.target.style.backgroundColor = color;
+        // or $(event.target).css("background-color",color);
     });
 });
